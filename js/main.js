653e0821c14131a219d745eab6cb265cc3dfba3b
dinosantic <dinos.mos@gmail.com>
2018-05-11 18:49:49 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var headerHeight = $("header").outerHeight();
+
+    $(".slide-section").click(function(e){
+       
+        var link = $(this).attr("href");
+       $("html, body").animate({
+           scrollTop: $(link).offset().top - headerHeight
+       }, 2000 );
+        e.preventDefault();
+    });
+});
+        var typed = new Typed('#typed', {
+            stringsElement: '#typed-strings',           
+            typeSpeed: 110,
+            backSpeed: 10,
+            fadeOut: true,
+            loop: true
+  });
+
